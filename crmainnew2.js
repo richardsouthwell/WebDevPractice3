eab515ef0c49827b07f43d0a9c92c2b0a16b9846
unknown <richardsouthwell254@gmail.com>
2020-07-02 23:47:09 +0100

    finished tutorial

--- crmainnew2.js
+++ crmainnew2.js
@@ -9,5 +9,30 @@ myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
-    console.log(nameInput.value)
+ //   console.log(nameInput.value)
+
+ if (nameInput.value === '' || emailInput.value === '') {
+     alert('Please enter fields');
+     // grabs new style (from css ?)
+     msg.classList.add('error');
+     msg.innerHTML = 'Please enter fields';
+     // set timeout, with removal function and time in milliseconds
+     setTimeout(() => msg.remove(), 3000);
+ } else {
+    // console.log('sucsess');
+    // if it works, we want to add data into ul (unordered list)
+    // create list item
+    const myli = document.createElement('li')
+
+    // add information inside of new element
+    myli.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+    userList.appendChild(myli);
+    // Clear fields 
+
+    nameInput.value = '';
+    emailInput.value = '';
+ }
 }
+
+// nodejs or fetch api, to store data
